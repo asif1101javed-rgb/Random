0276df001852b091a1c268a1abfbe01c246b4abf
Asif Ali <asif.1101.javed@gmail.com>
2026-08-29 17:09:07 +0500

    JavaScript

---
+++ script.js
@@ -0,0 +1,71 @@
+// Asif Ali
+
+// ES6+
+
+// Types of Programming 
+// 1) Procedural Programming
+
+// keyword
+// declarative keyword
+
+// Reassignment (Overwrites)
+// Redeclaration
+// Scope
+
+// var 
+// variable 
+
+// --------------------------------------------
+
+// var
+// var number = 2;
+// var number = 4;
+// 1) Reassignment (Overwrite)
+// 2) Can be Reassigned
+// 3) Can be Redeclared
+// 4) functional scope / global scope
+// number = 5;
+// console.log(number);
+
+// ---------------------------------------------
+
+// let
+// 1) Can be Reassigned
+// 2) Can't be Redeclared
+// let newvalue = 9
+// let newvalue = 10
+
+// ---------------------------------------------
+
+// const
+// 1) Can't be Reassigned
+// 2) Can't be Redeclared
+// const val = 56
+// val = 66;
+
+// --------------------------------------------
+
+// Global Scope
+
+// var number = 5
+ 
+// if(number == 5){
+//     var numb = 1
+//     console.log(numb)
+//     console.log(number);
+    
+// }
+// console.log(numb);
+// console.log(number);
+
+// ------------------------------------------
+
+// Functional Scope
+
+// var num = 2
+
+// function Sum() {
+//     var num = 5
+//     console.log(num);
+// }
+// console.log(num);
